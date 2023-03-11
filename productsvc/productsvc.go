2d50f140b5e9ec1cb9ba2b009e1774a4363fd90c
alextanhongpin <alextan220990@gmail.com>
2023-03-11 12:00:51 +0800

    feature(productsvc): add product service

---
+++ productsvc/productsvc.go
@@ -0,0 +1,44 @@
+package productsvc
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ServiceWeaver/weaver"
+	"github.com/alextanhongpin/restocknotif/adapter/postgres"
+)
+
+type T interface {
+	IncrementStock(ctx context.Context, productID, quantity int64) error
+}
+
+type repository interface {
+	IncrementStock(ctx context.Context, productID, quantity int64) error
+}
+
+type UseCase struct {
+	weaver.Implements[T]
+	repo repository
+}
+
+func (uc *UseCase) Init(ctx context.Context) error {
+	uc.Logger().Debug("Init")
+	uc.repo = NewRepository(postgres.New())
+
+	return nil
+}
+
+// IncrementStock increments the product stock by the given quantity.
+func (uc *UseCase) IncrementStock(ctx context.Context, productID, quantity int64) error {
+	uc.Logger().Debug("IncrementStock",
+		"productID", productID,
+		"quantity", quantity,
+	)
+
+	err := uc.repo.IncrementStock(ctx, productID, quantity)
+	if err != nil {
+		return fmt.Errorf("failed to increment stock: %w", err)
+	}
+
+	return nil
+}
---
+++ productsvc/repo.go
@@ -0,0 +1,33 @@
+package productsvc
+
+import (
+	"context"
+
+	"github.com/alextanhongpin/restocknotif/adapter/postgres"
+)
+
+type Repository struct {
+	db postgres.IDB
+}
+
+func NewRepository(db postgres.IDB) *Repository {
+	return &Repository{
+		db: db,
+	}
+}
+
+func (r *Repository) IncrementStock(ctx context.Context, productID, quantity int64) error {
+	p := postgres.Product{
+		Quantity: quantity,
+		ID:       productID,
+	}
+
+	_, err := r.db.NewUpdate().
+		Model(&p).
+		Column("quantity").
+		Set("quantity = quantity + ?quantity").
+		WherePK().
+		Exec(ctx)
+
+	return err
+}
